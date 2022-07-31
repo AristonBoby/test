be1c0cb64eeec890aaf39614a63f40dff04fe375
aristonboby <aristonbobi@gmail.com>
2022-08-01 07:32:52 +0800

    Pembuatan Laporan

--- app/Http/Livewire/Laporan.php
+++ app/Http/Livewire/Laporan.php
@@ -3,16 +3,22 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-use App\Models\kunjungan;
+use Illuminate\Support\Facades\DB;
+
 class Laporan extends Component
-{   public $umum;
+{   
     public function render()
     {
-        return view('livewire.Laporan.index');
-        
+        return view('livewire.Laporan.index');  
     }
 
     public function index(){
        return view('livewire.Laporan.index');
+    }
+    
+
+    public function laporanpoli(){
+        //$query = DB::table('kunjungans')->get();
+        dd('ddd');
     }
 }
