2ff8789b802003d3cfd5092236281c10222fc9ab
aristonboby <aristonbobi@gmail.com>
2022-08-01 18:49:06 +0800

    perbaikan form pengkajian

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -5,7 +5,7 @@
         <li class="nav-header">PENDAFTARAN PASIEN</li>
       <li class="nav-item">
         <a href="#" class="nav-link">
-          <i class="nav-icon fas fa-tachometer-alt"></i>
+          <i class="nav-icon fas fa-book"></i>
           <p>
             Data Pasien
             <i class="right fas fa-angle-left"></i>
@@ -24,21 +24,21 @@
               <p>Pasien Terdaftar</p>
             </a>
           </li>
-        
+
         </ul>
       </li>
       <li class="nav-item">
         <a href="#" class="nav-link">
-          <i class="nav-icon fas fa-th"></i>
+          <i class=" nav-icon fas fa-edit"></i>
           <p>
-            Input Kunjungan 
+            Input Kunjungan
             <i class="fas fa-angle-left right"></i>
           </p>
         </a>
         <ul class="nav nav-treeview">
           <li class="nav-item">
             <a href="{{route('Kunjungan')}}" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
+              <i class="far fa-circle nav-icon text-success"></i>
               <p>Kunjungan Sakit</p>
             </a>
           </li>
@@ -55,7 +55,7 @@
         <ul class="nav nav-treeview">
           <li class="nav-item">
             <a href="{{route('diagnosa')}}" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
+              <i class="far fa-circle nav-icon text-danger"></i>
               <p>Input Diagnosa</p>
             </a>
           </li>
@@ -63,7 +63,7 @@
       </li>
       <li class="nav-item">
         <a href="" class="nav-link">
-          <i class="nav-icon fas fa-th"></i>
+          <i class="nav-icon fas fa-table"></i>
           <p>
             Laporan
             <i class="fas fa-angle-left right"></i>
@@ -72,17 +72,21 @@
         <ul class="nav nav-treeview">
           <li class="nav-item">
             <a href="{{Route('laporan')}}" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Laporan</p>
+              <i class="far fa-circle nav-icon text-info"></i>
+              <p>Laporan Kunjungan Poli</p>
             </a>
+            <a href="{{Route('laporan')}}" class="nav-link">
+                <i class="nav-icon fas fa-chart-pie text-danger"></i>
+                <p>Laporan Chart</p>
+              </a>
           </li>
         </ul>
       </li>
-      
+
       <li class="nav-item">
-        
-       
+
+
       </li>
-    
+
     </ul>
   </nav>
