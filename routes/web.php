4536936fb2414f10494cfd1f8fc8407f684433a5
aristonboby <aristonbobi@gmail.com>
2022-08-07 23:42:21 +0800

    update route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Mailer\Transport\Smtp\Auth\LoginAuthenticator;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,19 +17,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Auth::routes();
+Auth::routes();//['register'=>false,'showLoginForm '=>false,'password.update'=>false,'reset'=>false,'confirm'=>false]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/pendaftaran',[App\Http\Livewire\Pendaftaran\Pasien\Components\Pasienbaru::class, 'index'])->name('pendaftaranPasien');
-Route::get('/updatePasien',[App\Http\livewire\pendaftaran\Pasien\Components\EditdataPasien::class,'index'])->name('updatePasien');
-Route::get('/printpasien/{id}',[App\Http\livewire\pendaftaran\Pasien\Cetak::class,'cetak'])->name('printPasien');
-Route::get('/kunjungan',[App\Http\livewire\pendaftaran\Kunjungan\index::class,'index'])->middleware('auth')->name('Kunjungan');
-
-
-
+Route::get('/updatePasien',[App\Http\Livewire\Pendaftaran\Pasien\Components\EditdataPasien::class,'index'])->name('updatePasien');
+Route::get('/printpasien/{id}',[App\Http\Livewire\Pendaftaran\Pasien\Cetak::class,'cetak'])->name('printPasien');
+Route::get('/kunjungan',[App\Http\Livewire\Pendaftaran\Kunjungan\Index::class,'index'])->middleware('auth')->name('Kunjungan');
+//Route::get('/',[LoginController::class,'showLoginForm'])->name('login');
 
 Route::middleware(['auth','admin'])->group(function(){
-    Route::get('/diagnosa',[App\Http\livewire\Diagnosa\index::class,'index'])->name('diagnosa');
-    Route::get('/tambahuser',[App\Http\Livewire\Tambahuser\index::class,'index'])->name('tambahuser');
+    Route::get('/diagnosa',[App\Http\Livewire\Diagnosa\Index::class,'index'])->name('diagnosa');
+    Route::get('/tambahuser',[App\Http\Livewire\Tambahuser\Index::class,'index'])->name('tambahuser');
 });
 Route::get('/laporan',[App\Http\Livewire\Laporan\home::class,'index'])->name('laporan');
 
