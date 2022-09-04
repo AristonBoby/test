fe772a26c1dffb0e4bcd7822cebd388852954fbb
aristonboby <aristonbobi@gmail.com>
2022-09-04 23:20:53 +0800

    update breadcrumb ubah password

--- resources/views/ubahpassword.blade.php
+++ resources/views/ubahpassword.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
-
+@section('breadcrumb')
+<div class="container-fluid">
+    <div class="row" style="margin-bottom:-5px;" >
+      <div class="col-sm-6 ">
+        <h5>UBAH PASSWORD</h5>
+      </div>
+      <div class="col-md-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item active">Ubah Password</li>
+      </ol>
+      </div>
+    </div>
+  </div><!-- /.container-fluid -->
+@endsection
 @section('content')
-    <livewire:ubahpassword.formubahpassword>
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <livewire:ubahpassword.formubahpassword>
+        </div>
+    </div>
+</div>
 @endsection
