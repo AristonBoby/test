198e3e3f8ce8d1b2dc7e74b6d8d7e3abbd7ca5a4
aristonboby <aristonbobi@gmail.com>
2023-02-17 21:50:02 +0800

    update

--- resources/views/livewire/pendaftaran/ptm/dataptm/index.blade.php
+++ resources/views/livewire/pendaftaran/ptm/dataptm/index.blade.php
@@ -22,6 +22,7 @@
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12">
                         <livewire:pendaftaran.ptm.dataptm.table-ptm>
+                         
                     </div>
                 </div>
         </div>
