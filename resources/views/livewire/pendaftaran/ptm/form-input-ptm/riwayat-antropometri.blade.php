802d1dac128c74242d47a7e2ed6d5c9d03fe08e3
aristonboby <aristonbobi@gmail.com>
2023-03-01 23:04:26 +0800

    update

--- resources/views/livewire/pendaftaran/ptm/form-input-ptm/riwayat-antropometri.blade.php
+++ resources/views/livewire/pendaftaran/ptm/form-input-ptm/riwayat-antropometri.blade.php
@@ -6,6 +6,37 @@
       </div>
     </div>
     <div class="card-body" wire:loading.remove>
-
+        <div class="table-responsive">
+            <table class="table table-xs table-bordered text-xs text-uppercase">
+                <thead>
+                    <tr>
+                        <th class="text-center">No</th>
+                        <th class="text-center">Tanggal</th>
+                        <th class="text-center">Sistole</th>
+                        <th class="text-center">Diastole</th>
+                        <th class="text-center">Tinggi Badan</th>
+                        <th class="text-center">Berat Badan</th>
+                        <th class="text-center">Lingkar Perut</th>
+                        <th class="text-center">Glukosa</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                @if(!empty($query))
+                    @foreach($query as $i=>$data)
+                        <tr>
+                            <td>{{$i+1}}.</td>
+                            <td>{{\carbon\carbon::parse($data->tanggal)->format('d-m-Y')}}</td>
+                            <td>{{$data->sistole}}</td>
+                            <td>{{$data->diastole}}</td>
+                            <td>{{$data->tinggi_badan}}</td>
+                            <td>{{$data->berat_badan}}</td>
+                            <td>{{$data->lingkar_perut}}</td>
+                            <td>{{$data->glukosa}}</td>
+                        </tr>
+                    @endforeach
+                @endif
+                </tbody>
+            </table>
+        </div>
     </div>
   </div>
